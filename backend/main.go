a4edb54f1a8d04a3fe0bd93f987ede6f6dcc5bb4
Aidan Matchette <aidan.j.matchette@gmail.com>
2023-02-23 20:46:52 -0800

    basic messaging connection completed

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+
+    CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func reader(conn *websocket.Conn) {
+    for {
+        messageType, p, err := conn.ReadMessage()
+        if err != nil {
+            log.Println(err)
+            return
+        }
+
+        fmt.Println(string(p))
+
+        if err := conn.WriteMessage(messageType, p); err != nil {
+            log.Println(err)
+            return
+        }
+    }
+}
+
+func serveWebSockets(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r.Host)
+
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+    }
+
+    reader(ws)
+}
+
+func setupRoutes() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w,"Simple Server")
+    })
+
+    http.HandleFunc("/ws", serveWebSockets)
+}
 
 func main() {
-    fmt.Println("Go Chat App V1")
+    setupRoutes()
+    fmt.Println("Starting server on port 8080...")
+    http.ListenAndServe(":8080", nil)
+    
 }
